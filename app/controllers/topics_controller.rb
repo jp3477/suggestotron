a3f732f32df45323ee4cb4f044a8923398377856
Jason Patterson <jason.patterson@columbiaspectator.com>
2015-02-14 13:07:41 -0500

    Added more voting functionality

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -65,6 +65,8 @@ class TopicsController < ApplicationController
     @topic = Topic.find(params[:id])
     @topic.votes.create
     redirect_to(topics_path)
+  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
